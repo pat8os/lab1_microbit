1a1a71ef00ec84c3b7cf3c5858d25d42de3c827b
pat8os <119261648+pat8os@users.noreply.github.com>
2026-03-10 09:47:12 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
-let noise = 0
-let light2 = 0
-led.enable(false)
-let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
+let temp = 0
+OLED.init(128, 64)
 basic.forever(function () {
-    light2 = smarthome.ReadLightIntensity(AnalogPin.P3)
-    if (light2 < 50) {
-        noise = smarthome.ReadNoise(AnalogPin.P2)
-        if (noise > 70) {
-            strip.showColor(neopixel.colors(NeoPixelColors.Purple))
-            basic.pause(1000)
-            strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        }
-    }
+    temp = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P1)
+    OLED.clear()
+    OLED.writeString("Temperature")
+    OLED.writeNum(temp)
 })
