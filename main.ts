16fd3d9168edef243b8afaa07d673e683502bed6
pat8os <119261648+pat8os@users.noreply.github.com>
2026-03-10 10:23:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
-let temp = 0
-OLED.init(128, 64)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+let door = false
+let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
 basic.forever(function () {
-    temp = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P1)
-    OLED.clear()
-    OLED.writeString("Temperature")
-    OLED.writeNum(temp)
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
+        door = !(door)
+        if (door == true) {
+            strip.showColor(neopixel.colors(NeoPixelColors.White))
+            pins.servoWritePin(AnalogPin.P7, 0)
+            basic.pause(1000)
+        } else {
+            pins.servoWritePin(AnalogPin.P7, 180)
+            basic.pause(1000)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        }
+    }
 })
